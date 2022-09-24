54af4e0ca51e681b581d3a2b32770aae1dd09315
samrat singh <114308768+singhsamrat999@users.noreply.github.com>
2022-09-24 23:57:03 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int 12,21,area;
+    clrscr();
+printf("enter value of 12=");
+scanf("%d",&12);
+printf("enter value of 21=");
+scanf("%d",&21);
+area=12*21;
+printf("area=%d",area);
+return0;
+}
