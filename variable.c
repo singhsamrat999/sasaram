38243d309173da9be287a3209e12083a791797d9
samrat singh <114308768+singhsamrat999@users.noreply.github.com>
2022-09-25 00:12:49 +0530

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a=8;
+    float b=8.5;
+    char c='u';
+    printf("the value of a is %d \n",a);
+    printf("the value of float is %f \n",b);
+    return 0;
+
+
+
+}
